ae5eba3f5af1c8f15920836c1acea4e925ef26fd
Luiz Gustavo <gustavo180g@gmail.com>
2018-05-23 18:51:10 -0300

    Feito até a C da 5(21/05/2018)

--- vetores.py
+++ vetores.py
@@ -36,3 +36,85 @@
 # while p < len(lista):
 #     print(lista[p], end=', ') # função end coloca os nºs lado a lado
 #     p+=1
+
+# # 3)Um professor precisa armazenar em uma lista os nomes dos alunos presentes em um minicurso. Faça um programa para armazenar
+# # 5 nomes e permitir que o professor digite o nome da cada um. Em seguida, apresente na tela todos os nomes informados.
+# lista = [""]*5
+# p = 0
+# while p < len(lista):
+#     lista[p] = str(input("Insira o seu nome:"))
+#     p+=1
+#
+# p=0
+# print("------------------------")
+# print("Os nomes dos aluno são:")
+# while p < len(lista):
+#     print(lista[p])
+#     p+=1
+
+# # 4)Faça um programa que peça para o usuário informar o tamanho de um vetor. Em seguida, crie este vetor e
+# # preencha ele com números digitados pelo usuário. Por fim, apresente na tela os números digitados.
+#
+# vetor = int(input("Insira o tamanho do vetor: "))
+# lista = [""]* vetor
+# p = 0
+#
+# while p < len(lista):
+#     lista[p] = int(input("Agora digite um número: "))
+#     p+=1
+#
+# print("-----------------------------")
+# p= 0
+# while p < len(lista):
+#     print("Você digitou: ",lista[p])
+#     p+=1
+
+# #5)
+# #a)Apresente os números do vetor em ordem inversa.
+# vetor = int(input("Insira o tamanho do vetor: "))
+# lista = [""]* vetor
+# p = len(lista) - 1
+#
+# while p >= 0:
+#     lista[p] = int(input("Agora digite um número: "))
+#     p-=1
+#
+# print("-----------------------------")
+# p= 0
+# while p < len(lista):
+#     print("Você digitou: ",lista[p])
+#     p+=1
+
+
+# #B) Apresente a soma de todos os elementos.
+# vetor = int(input("Insira o tamanho do vetor: "))
+# lista = [""]* vetor
+# p = 0
+#
+# while p < len(lista):
+#     lista[p] = int(input("Agora digite um número: "))
+#     p+=1
+#
+# print("-----------------------------")
+# soma = 0
+# p = 0
+# while p < len(lista):
+#     soma += lista[p]
+#     p += 1
+# print("A soma total é",soma)
+#c) Calcule a média aritmética dos valores.
+vetor = int(input("Insira o tamanho do vetor: "))
+lista = [""]* vetor
+p = 0
+
+while p < len(lista):
+    lista[p] = int(input("Agora digite um número: "))
+    p+=1
+
+print("-----------------------------")
+soma = 0
+p = 0
+while p < len(lista):
+    soma += lista[p]
+    p += 1
+print("a média é",soma/len(lista))
