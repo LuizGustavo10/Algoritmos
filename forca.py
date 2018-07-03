58b29714700c562ad645e376b31bbb7005a58115
LucasAparecidoFlorentino <37313637+LucasAparecidoFlorentino@users.noreply.github.com>
2018-07-03 12:31:34 -0300

    Novas palavras e dicas

--- forca.py
+++ forca.py
@@ -1,16 +1,19 @@
 from random import randint
 listaPalavras = [
-"arvore","casa" ]
+"arvore","casa", "mercedes", "fitness", "fortnite", "mustang", "zebra", ]
 
 listaDicas = [
-"tem folhas...","serve de abrigo..." ]
+"tem folhas...","serve de abrigo...", "marca de carro...", "academia...", "um jogo battle royale...", "um muscle car...", "um animal..." ]
 
 # Escolhe uma palavra da lista
-x = randint(0,len(listaPalavras) - 1) # é -1 porque a lista começa em 0, se não tiver o -1 vai passar o tanto de posições
+x = randint(0,len(listaPalavras) - 1)
+z = randint(0,len(listaDicas) - 1) # é -1 porque a lista começa em 0, se não tiver o -1 vai passar o tanto de posições
 escolhida = listaPalavras[x]
+escolhida_dicas = listaDicas[z]
 
 #isso vai separar as letras da palavra, será usado em breve
 palavra =[escolhida]
+dica = [escolhida_dicas]
 
 for c in range(0,len(escolhida)):
     print(escolhida[c])
