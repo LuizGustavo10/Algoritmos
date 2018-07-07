c4e261dfdf5f9f66a5332278f69bfd4e6bd8c0ec
Luiz Gustavo <gustavo180g@gmail.com>
2018-07-07 17:19:09 -0300

    forca aprimorada com funções def, funcionando normalmente

--- Função Aleatória-aula16.py
+++ Função Aleatória-aula16.py
@@ -1,50 +1,50 @@
-from random import randint
-# Criação do vetor para armazenar números
-loteria = [""]*6
-# Variável de controle do laço de repetição
-i = 0
-# Laço para preencher o vetor
-while i < len(loteria):
-    # Gera número
-    num = randint(1,60)
-    # Variável para saber se repetiu ou não
-    repetiu = "n"
-
-    # Variável de controle do laço que verifica se o número já existe ou não
-    k = 0
-    # Laço para verificação do número
-    while k < i:
-        # Verifica se o núm está na posição k
-        if num == loteria[k]:
-            # Se for verdadeiro, o número já está lá
-            # Diminui o valor do "i" para fazer este processo novamente
-            i -= 1
-            # Muda o valor da variável repetiu
-            repetiu = "s"
-            # Parar o laço de repetição
-            break
-
-        # Incremento da posição
-        k += 1
-        # Fim do while do k
-
-    if repetiu == "n":
-        # Armazena
-        loteria[i] = num
-
-    # Incremento da posição
-    i += 1
-
-# Mostrar a lista gerada
-print(loteria)
-
-numeros = []
-
-while len(numeros) < 6
-num = randint(1,60)
-if num not in numeros:
-    numeros.append(num)
-print(numeros)
+# from random import randint
+# # Criação do vetor para armazenar números
+# loteria = [""]*6
+# # Variável de controle do laço de repetição
+# i = 0
+# # Laço para preencher o vetor
+# while i < len(loteria):
+#     # Gera número
+#     num = randint(1,60)
+#     # Variável para saber se repetiu ou não
+#     repetiu = "n"
+#
+#     # Variável de controle do laço que verifica se o número já existe ou não
+#     k = 0
+#     # Laço para verificação do número
+#     while k < i:
+#         # Verifica se o núm está na posição k
+#         if num == loteria[k]:
+#             # Se for verdadeiro, o número já está lá
+#             # Diminui o valor do "i" para fazer este processo novamente
+#             i -= 1
+#             # Muda o valor da variável repetiu
+#             repetiu = "s"
+#             # Parar o laço de repetição
+#             break
+#
+#         # Incremento da posição
+#         k += 1
+#         # Fim do while do k
+#
+#     if repetiu == "n":
+#         # Armazena
+#         loteria[i] = num
+#
+#     # Incremento da posição
+#     i += 1
+#
+# # Mostrar a lista gerada
+# print(loteria)
+#
+# numeros = []
+#
+# while len(numeros) < 6
+# num = randint(1,60)
+# if num not in numeros:
+#     numeros.append(num)
+# print(numeros)
 # vetor com varias palavras aleatorias
 #desenhar as forkas
 #usar o que a gente usou
@@ -86,70 +86,41 @@ print(numeros)
 # print("""
 #
 # """)
-#Exemplo de Forca abaixo
-palavraSecreta = ["L","U","I","Z"]
-letrasDescobertas = []
-
-print("---------Jogo da Forca----------")
-
-for i in range(0, len(palavraSecreta)):  #é como se fosse o p = p + 1, vai até o tamanho da palavra
-    letrasDescobertas.append("_") #coloca o tracinho no lugar de cada letra
-
-acertou = False
-
-while acertou == False: #Enquanto ele não acertou
-    letra = input("dígite a letra: ")
-
-    for i in range(0, len(palavraSecreta)): #percorre cada uma das letras da palavraSecreta, para verificar se a letra é correta
-        if letra == palavraSecreta[i]: #compara a cada uma das letras de palavra secreta
-            letrasDescobertas[i].append(letra) #substitui o tracinho pela letra
-
-        print(letrasDescobertas[i])
-
-    acertou = True #abaixo vai verificar as letras que foram descobertas
-    for p in range(0, len(letrasDescobertas)):
-        if letrasDescobertas[p] == "_":
-            acertou = False
+# #Exemplo de Forca abaixo
+# palavraSecreta = ["L","U","I","Z"]
+# letrasDescobertas = []
+#
+# print("---------Jogo da Forca----------")
+#
+# for i in range(0, len(palavraSecreta)):  #é como se fosse o p = p + 1, vai até o tamanho da palavra
+#     letrasDescobertas.append("_") #coloca o tracinho no lugar de cada letra
+#
+# acertou = False
+#
+# while acertou == False: #Enquanto ele não acertou
+#     letra = input("dígite a letra: ")
+#
+#     for i in range(0, len(palavraSecreta)): #percorre cada uma das letras da palavraSecreta, para verificar se a letra é correta
+#         if letra == palavraSecreta[i]: #compara a cada uma das letras de palavra secreta
+#             letrasDescobertas[i].append(letra) #substitui o tracinho pela letra
+#
+#         print(letrasDescobertas[i])
+#
+#     acertou = True #abaixo vai verificar as letras que foram descobertas
+#     for p in range(0, len(letrasDescobertas)):
+#         if letrasDescobertas[p] == "_":
+#             acertou = False
 
 #funções
 
 # estudar funções
 #inicio
 def mostrarErro1():
-    # mostrar/pedir numeros, sempre use um verbo no def
+    # mostrar/pedir numeros, sempre use um verbo no
+    print("Oloco")
 
-# seu códifo lá em baixo:
+    # seu códifo lá em baixo:
 erro=1
 
 if erro == 1:
-    def mostrarErro1():
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#
+    mostrarErro1()
--- forca.py
+++ forca.py
@@ -1,132 +1,109 @@
 from random import randint
+#relativo aos desenhos da forca, várias funções, o código está abaixo dos desenhos
+def erro1():
+    print("""
 
-reiniciar = 1
-while reiniciar == 1:
-    listaPalavras = [
-    "arvore","casa", "mercedes", "fitness", "fortnite", "mustang", "zebra" ]
+|─|─────────────────|
+| |               (--)
+| |
+| |
+| |
+| |
+| |
+| |
+|_|______________________
+você tem 6 tentativas
+─────────────────────────
+""")
+def erro2():
+    print("""
 
-    listaDicas = [
-    "tem folhas...","serve de abrigo...", "marca de carro...", "academia...", "um jogo battle royale...", "um muscle car...", "um animal..." ]
+|─|─────────────────|
+| |               (oo)
+| |                ||
+| |                ||
+| |                ||
+| |
+| |
+| |
+|_|______________________
+você tem 5 tentativas
+─────────────────────────
+""")
+def erro3():
+    print("""
 
+|─|─────────────────|
+| |               (oo)
+| |                ||_
+| |                || \\
+| |                ||  \\
+| |
+| |
+| |
+|_|______________________
+você tem 4 tentativas
+─────────────────────────
+""")
+def erro4():
+    print("""
 
-    # Escolhe uma palavra da lista
-    x = randint(0,len(listaPalavras) - 1) # é -1 porque a lista começa em 0, se não tiver o -1 vai passar o tanto de posições, aqui estou trabalhando com posições
+|─|─────────────────|
+| |               (oo)
+| |               _||_
+| |              / || \\
+| |             /  ||  \\
+| |
+| |
+| |
+|_|______________________
+você tem 3 tentativas
+─────────────────────────
+""")
+def erro5():
+    print("""
 
-    escolhida = listaPalavras[x]                    # o "X" refere-se a posição escolhida
+|─|─────────────────|
+| |               (oo)
+| |               _||_
+| |              / || \\
+| |             /  ||  \\
+| |                /
+| |              _/
+| |
+|_|______________________
+você tem 2 tentativas
+─────────────────────────
+""")
+def erro6():
+    print("""
 
-    #isso vai separar as letras da palavra, será usado em breve
-
-    descobertas = []
-    digitadas = []
-
-    for c in range(0,len(escolhida)): #essa parte percorre cada letra da palavra escolhida aleatoriamente, uma palavra pode ser uma lista...
-        descobertas.append("_") #append ??????????????????????????
-
-    print(" _" * len(escolhida)) #Esse é apenas o print Inicial, antes do laço de repetição
-    print(listaDicas[x])
-
-    acertou = False
-    erros = 0
-
-    while acertou == False:
-        acertos = 0
-        print(" ")
-        letra = input(str("Dígite uma letra: "))
-
-        if letra in digitadas:
-            print("Você já tentou essa letra !!") #verifica se a letra ja foi digitada, ainda falta arrumar
-        else:
-
-            digitadas.append(letra)
-
-            print(listaDicas[x])
-
-
-            for c in range(0, len(escolhida)):
-
-
-                if letra == escolhida[c]:
-                    descobertas[c] = letra
-                    acertos+=1
-
-                print(descobertas[c], end=' ')
-
-            if acertos == 0:  #contador de erros, se não tiver nenhum acerto na rodada, mais um erro
-                erros+=1
-
-
-            if erros == 1:
-                print("""
-        Você errou !
-        ------
-        |    O
-        |
-        |
-        |
-                """)
-
-            elif erros == 2:
-                print("""
-        Você errou !
-        ------
-        |    O
-        |   /
-        |
-        |
-                """)
-
-            elif erros == 3:
-                print("""
-        Você errou !
-        ------
-        |    O
-        |   / \\
-        |
-        |
-            """)
-
-            elif erros == 4:
-                print("""
-        Você errou !
-        ------
-        |    O
-        |   /|\\
-        |
-        |
-        |
-        """)
-
-            elif erros == 5:
-                print("""
-        Você errou !
-        ------
-        |    O
-        |   /|\\
-        |   /
-        |
-        """)
-
-            elif erros == 6:
-                print("""
-        Você errou !
-        ------
-        |    O
-        |   /|\\
-        |   / \\
-        |
-                """)
-
-            if erros == 7:
-                print("""
-        Você errou !
-        ------
-        |    O
-        |   /|\\
-        |   / \\
-        |
-                """)
-                print("Enforcado")
-                print('''
+|─|─────────────────|
+| |               (oo)
+| |               _||_
+| |              / || \\
+| |             /  ||  \\
+| |                /\\
+| |              _/  \\_
+| |
+|_|_______________________
+você só tem 1 tentativa!!!
+──────────────────────────
+""")
+def ganhou():
+    print('''
+----Parabéns!!!!------
+───▐▀▄─────────▄▀▌───
+───▐▓░▀▄▀▀▀▀▀▄▀░▓▌───
+───▐░▓░▄▀░░░▀▄░▓░────
+────█░░▌█▐░▌█▐░░█────
+─▄▄▄▐▀░░░▀█▀░░░▀▌▄▄▄─
+█▐▐▐▌▀▄░▀▄▀▄▀░▄▀▐▌▌▌█
+▀▀▀▀▀▀▀▀▀▄▄▄▀▀▀▀▀▀▀▀▀
+───Você ganhou !!!───
+─────────────────────''')
+def erro7():
+    print('''
 ███▀▀▀██┼███▀▀▀███┼███▀█▄█▀███┼██▀▀▀
 ██┼┼┼┼██┼██┼┼┼┼┼██┼██┼┼┼█┼┼┼██┼██┼┼┼
 ██┼┼┼▄▄▄┼██▄▄▄▄▄██┼██┼┼┼▀┼┼┼██┼██▀▀▀
@@ -150,8 +127,67 @@ while reiniciar == 1:
 ┼┼┼┼┼┼┼┼▄▄▄██┼┼█▀█▀█┼┼██▄▄▄┼┼┼┼┼┼┼┼┼
 ┼┼┼┼┼┼┼┼▀▀██┼┼┼┼┼┼┼┼┼┼┼██▀▀┼┼┼┼┼┼┼┼┼
 ┼┼┼┼┼┼┼┼┼┼▀▀┼┼┼┼┼┼┼┼┼┼┼▀▀┼┼┼┼┼┼┼┼┼┼┼''')
-                break
+#inicio do código da forca...
+reiniciar = 1
+while reiniciar == 1:
+    listaPalavras = [
+    "arvore","casa", "mercedes", "fitness", "fortnite", "mustang", "zebra","dinossauro","laranja","minecraft"]
+    listaDicas = [
+    "tem folhas...","serve de abrigo...", "marca de carro...", "academia...", "um jogo battle royale...", "um muscle car...", "um animal...","Um animal já extinto...","Uma cor...","Um jogo de construções..."]
 
+    # Escolhe uma palavra da lista
+    x = randint(0,len(listaPalavras) - 1) # é -1 porque a lista começa em 0, se não tiver o -1 vai passar o tanto de posições, aqui estou trabalhando com posições
+    escolhida = listaPalavras[x] # o "X" refere-se a posição escolhida
+
+    #isso vai separar as letras da palavra, será usado em breve
+    descobertas = []
+    digitadas = []
+
+    for c in range(0,len(escolhida)): #essa parte percorre cada letra da palavra escolhida aleatoriamente, uma palavra pode ser uma lista...
+        descobertas.append("_") #append ??????????????????????????
+
+    print(" _" * len(escolhida)) #Esse é apenas o print Inicial, antes do laço de repetição
+    print(listaDicas[x])
+
+    acertou = False
+    erros = 0
+
+    while acertou == False:
+        acertos = 0
+        print(" ")
+        letra = input(str("Dígite uma letra: "))
+
+        if letra in digitadas:
+            print("Você já tentou essa letra !!") #verifica se a letra ja foi digitada, ainda falta arrumar
+        else:
+            digitadas.append(letra)
+            print(listaDicas[x])
+
+            for c in range(0, len(escolhida)):
+                if letra == escolhida[c]:
+                    descobertas[c] = letra
+                    acertos+=1
+
+                print(descobertas[c], end=' ')
+
+            if acertos == 0:  #contador de erros, se não tiver nenhum acerto na rodada, mais um erro
+                erros+=1
+
+            if erros == 1:
+                erro1() #isso é uma função, relaciona com o desenho lá em cima
+            elif erros == 2:
+                erro2()
+            elif erros == 3:
+                erro3()
+            elif erros == 4:
+                erro4()
+            elif erros == 5:
+                erro5()
+            elif erros == 6:
+                erro6()
+            if erros == 7:
+                erro7()
+                break
 
             acertou = True
             for z in range(0, len(descobertas)):
@@ -160,19 +196,8 @@ while reiniciar == 1:
 
     if erros < 7:
         print(" ")
-        print('''
-Parabéns!!!!
-───▐▀▄─────────▄▀▌───
-───▐▓░▀▄▀▀▀▀▀▄▀░▓▌───
-───▐░▓░▄▀░░░▀▄░▓░─────
-────█░░▌█▐░▌█▐░░█─────
-─▄▄▄▐▀░░░▀█▀░░░▀▌▄▄▄
-█▐▐▐▌▀▄░▀▄▀▄▀░▄▀▐▌▌▌█
-▀▀▀▀▀▀▀▀▀▄▄▄▀▀▀▀▀▀▀▀▀
-───Você ganhou !!!───
-─────────────────────
-''') #se o erro for < 7 dá o parabéns
-
+        ganhou()
+        #se o erro for < 7 dá o parabéns
     reiniciar = int(input("Insira (1) para jogar novamente e (2) para sair: "))
     if reiniciar == 2:
         break           #se o usuário escolher(2) sai do programa
